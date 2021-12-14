82a5c8acfebf300c9b29db5513932827ae5aa98d
Fernando Jerez <fernandojerez@gmail.com>
2021-12-13 19:36:58 -0500

    Add benefits and pandas to requirements

--- load_levels.py
+++ load_levels.py
@@ -10,8 +10,8 @@ engine = create_engine(
 
 positions_df = pd.read_sql_query("""
 select c.name company_name,
-       p.name position,
-       level ladder_level,
+    p.name position,
+    level ladder_level,
 case instr(salary, 'k')
 when 0 then cast(replace(replace(salary, '$', ''), 'm', '') as decimal) * 1000
 else cast(replace(replace(salary, '$', ''), 'k', '') as decimal)
@@ -21,3 +21,10 @@ inner join salary s on p.id = s.position
 inner join company c on p.company = c.id
 order by 4 desc
 """, engine)
+
+benefits_df = pd.read_sql_query("""
+select c.name company_name, b.name benefit
+from benefit b
+inner join company c on b.company = c.id
+""", engine)
+
